2322469dada6b5ed5a7ae358c72b32b7ff9668b3
bihuibin <bihuibin@163.com>
2019-02-28 00:04:13 +0800

    左侧自动展开菜单完成

---
+++ src/Pages/user/index.jsx
@@ -0,0 +1,13 @@
+import React,{Component} from 'react'
+import ReactDOM from 'react-dom'
+
+export default class User extends Component{
+
+  render(){
+
+    return (
+      <dev>user</dev>
+    )
+  }
+
+}
