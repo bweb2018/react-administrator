ea679d953b5fe60b51d01aa77a5df7d8066bd4c5
bihuibin <bihuibin@163.com>
2019-02-27 23:58:51 +0800

    完成admin页面左侧导航自动生成以及自动展开菜单功能

---
+++ src/Pages/role/index.jsx
@@ -0,0 +1,13 @@
+import React,{Component} from 'react'
+import ReactDOM from 'react-dom'
+
+export default class Role extends Component{
+
+  render(){
+
+    return (
+      <dev>Role</dev>
+    )
+  }
+
+}
